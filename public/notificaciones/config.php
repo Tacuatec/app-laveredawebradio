881d25a653364d7939f7d5ffd335d3ad728b5515
Esteban Sanjurjo <41419364+webma5ter23@users.noreply.github.com>
2022-09-23 04:06:14 -0300

    initial commit

---
+++ public/notificaciones/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $fcm_server_key = 'AAAA3hRYBUs:APA91bHSqmtvIrxGg0gJffIgfAz82whJJNWIIeW2iMyA10HiFonqjEIBreF76VI4LfDCF95B9qsA2FZpgQmAuQO_bibGqXzaRjkIygajN_wSbWFLSrCR0oktIbWeCvNf1oVMoXDXQ2Vf';
+
+    $fcm_topic = 'lavereda_radio_topic';
+
+    $onesignal_app_id = 'c2744cf7-7441-4693-9b98-c8a5e0d2fe81';
+    
+    $onesignal_rest_api_key = 'ZWVlYzIwNjctOWRhZS00NDViLTgyYWQtZDFjMTM5ZmM4MTZk';
+
+?>
